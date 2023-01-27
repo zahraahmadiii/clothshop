e36fbb862ec6cf71310abcbeb237390f35d4c700
yeganehNobakht <m.yeganeh.n1994@gmail.com>
2023-01-27 12:23:11 +0330

    feat: checkout form

---
+++ my-project/src/Components/checkoutform/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './style.css';
+
+const CheckoutForm = () => {
+
+    return (
+
+        <form>
+            <label htmlFor="email">email</label>
+            <input type="email" id="email" />
+            <label htmlFor="name">name</label>
+            <input type="text" id="name" />
+            <label htmlFor="address">address</label>
+            <input type="text" id="address" />
+
+            <input type="button" className="btn" />
+        </form>
+    )
+}
+export default CheckoutForm
